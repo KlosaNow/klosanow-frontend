5d6efddc1e09f5b4ea86c6524e0f64e0876562e8
sammy-code98 <sammysmart98@yahoo.com>
2023-04-18 12:47:02 +0100

    add splashscreen

--- src/pages/ErrorPage/Error.tsx
+++ src/pages/ErrorPage/Error.tsx
@@ -1,7 +1,9 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Text } from '@chakra-ui/react'
 
 export default function Error() {
     return (
-        <Box display='flex' justifyContent='center' alignItems='center'>Error page</Box>
+        <Box display='flex' justifyContent='center' alignItems='center' h='100vh'>
+            <Text fontSize='3xl' color='primary.70' text-bold>Error page</Text>
+        </Box>
     )
 }
--- src/pages/SplashScreen/SplashScreen.tsx
+++ src/pages/SplashScreen/SplashScreen.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import { Box, Image } from '@chakra-ui/react'
-
+import { Box, Center, Image } from '@chakra-ui/react'
+import SplashImg from "../../assets/SplashScreenImg/SplashLogo.png"
 
 export default function SplashScreen(): JSX.Element {
     return (
-        <Box>SplashScreen</Box>
+        <Box >
+            <Center h='100vh'>
+                <Image src={SplashImg} alt='klosanow logo' />
+            </Center>
+        </Box>
     )
 }
