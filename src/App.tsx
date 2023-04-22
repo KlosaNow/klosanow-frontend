78cc4cceea7804b2ec12b35caacf11ec7fc89229
Nelson Elaye <nelsonelaye@gmail.com>
2023-04-22 19:30:02 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom"
 import { Error } from "./pages/ErrorPage";
+import { SplashScreen } from './pages/SplashScreen';
 import Onboarding from "./pages/Onboarding";
 
 function App() {
   return (
     <>
       <Routes>
+       <Route path="/" element={<SplashScreen />} />
         <Route path="/onboarding" element={<Onboarding />} />
         <Route path="*" element={<Error />} />
       </Routes>
