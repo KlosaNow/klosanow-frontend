0f4ec4f3dfe92483f79ec4b45b061a234b613ed2
onotaizee@gmail.com <onotaizee@gmail.com>
2023-04-22 18:28:44 +0100

    feat: notifications, notifications settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom"
 import { SplashScreen } from './pages/SplashScreen';
 import { Error } from "./pages/ErrorPage";
+import { Notifications } from "./pages/Notifications";
+import { NotificationSettings } from "./pages/NotificationSettings";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<SplashScreen />} />
+        <Route path="/notifications" element={<Notifications />} />
+        <Route path="/settings/notifications" element={<NotificationSettings />} />
         <Route path="*" element={<Error />} />
       </Routes>
 
---
+++ src/pages/NotificationSettings/notification-settings.tsx
@@ -0,0 +1,43 @@
+import { Spacer, Box, Text, Switch } from '@chakra-ui/react'
+import { ArrowBackIcon } from '@chakra-ui/icons'
+
+const padding = '1.5em'
+
+export default function NotificationSettings() {
+	return(
+		<Box>
+			<Box display='flex' justifyContent='start' alignItems="center" h='2em'>
+				<ArrowBackIcon boxSize={6} mr='13px'/>
+				<Text fontSize='18px' fontWeight={500}>Notifications</Text>
+			</Box>
+			<Box h="20em">
+				<Box display="flex" justifyContent='space-between' p={padding}>
+					<Text fontWeight={400} >General</Text>
+					<Switch size='md' colorScheme='purple'/>
+				</Box>
+
+				<Spacer/>
+
+				<Box display="flex" justifyContent='space-between' p={padding}>
+					<Text fontWeight={400}>Channels</Text>
+					<Switch size='md' colorScheme='purple'/>
+				</Box>
+
+				<Spacer/>
+
+				<Box display="flex" justifyContent='space-between' p={padding}>
+					<Text fontWeight={400}>Study Chats</Text>
+					<Switch size='md' colorScheme='purple'/>
+				</Box>
+
+				<Spacer/>
+
+				<Box display="flex" justifyContent='space-between' p={padding}>
+					<Text fontWeight={400}>Subscriptions</Text>
+					<Switch size='md' colorScheme='purple'/>
+				</Box>
+
+			</Box>
+		</Box>
+	)
+}
