ca4edd60fa907e917ec65c62969fa76dc69e3c0a
Solomon Great <greatsoma2019@gmail.com>
2023-04-23 20:42:02 +0100

    fix: Improved slideshow UX

--- src/pages/OnbordingSlides/OnboardingSlides.tsx
+++ src/pages/OnbordingSlides/OnboardingSlides.tsx
@@ -2,7 +2,8 @@ import { useState,useEffect } from "react";
 import { SlideTemplate } from "./SlideTemplate";
 import { useNavigate } from "react-router-dom";
 import { AnimatePresence, motion } from "framer-motion";
-import { Button, Stack } from "@chakra-ui/react";
+import { Stack } from "@chakra-ui/react";
+import {BTN} from '../../components/Button'
 
 interface OnboardingSlidesProps {
   slides: { image: string; title: string; description: string; id: string }[];
@@ -34,28 +35,28 @@ const OnboardingSlides = ({ slides }: OnboardingSlidesProps) => {
     }
   };
   return (
-    <AnimatePresence mode='popLayout'>
+    <AnimatePresence mode='wait'>
       <motion.div
         key={currentIndex}
-        initial={{ translateX: 200 }}
-        animate={{ translateX: 0 }}
+        initial={{ translateX: 200 , opacity: 0, scale: 0.9}}
+        animate={{ translateX: 0 ,opacity: 1 ,scale: 1}}
         exit={{ translateX: -400 ,scale: 0.9, opacity: 0 }}
         transition={{ delay: 0, duration: 0.5, damping: 80, stiffness: 10 }}
       >
         <SlideTemplate {...slides[currentIndex]} />
       </motion.div>
       <Stack direction='column' align='center'>
-      <Button variant='solid' color='neutral.50' background='primary.50' w='85%' h='50px' onClick={handleNextSlide}>
+      <BTN variant='solid' color='neutral.50' backgroundColor='primary.50' width='85%' height='50px' action={handleNextSlide}>
         {currentIndex === slides.length - 1 ? "Login" : "Next"}
-      </Button>
-      <Button
+      </BTN>
+      <BTN
       variant='ghost'
       color='#7B58F4'
-        onClick={() => navigate("/login")}
-        disabled={currentIndex === slides.length - 1}
+      action={() => navigate("/login")}
+      backgroundColor='transparent.100' width='85%' height='50px'
       >
         {currentIndex === slides.length - 1 ? "Sign into your account" : "skip"}
-      </Button>
+      </BTN>
     </Stack>
     </AnimatePresence>
   );
