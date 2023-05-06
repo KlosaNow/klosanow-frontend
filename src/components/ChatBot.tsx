520643d440cb4a16c14594924c417ce27f78318e
Toma John Musa <tomajohn316@yahoo.com>
2023-05-06 08:53:29 +0100

    Add text field in chat bot component

--- src/components/ChatBot.tsx
+++ src/components/ChatBot.tsx
@@ -1,7 +1,23 @@
-function ChatBot() {
-  return (
-    <>Chat with me</>
-  )
-}
+import Heading from "./Heading";
+import { BsSend } from "react-icons/bs";
+
+const ChatBot = () => (
+  <>
+    <Heading pageName="Chat with Bot" />
+
+    <section className="chat">
+      <div>
+
+      </div>
+
+      <div className="chat-field">
+        <input type="text" placeholder="Type your message" required />
+        <button className="icon">
+          <BsSend />
+        </button>
+      </div>
+    </section>
+  </>
+);
 
 export default ChatBot;
