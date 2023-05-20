b5f32795dcc4595dfe5d1f30ad3a69af7a8b9b74
Odafe <alaiyaodafe@gmail.com>
2023-05-20 23:33:44 +0100

    Fixed minor bugs

--- src/pages/StudyChat/MainPage/MainPage.tsx
+++ src/pages/StudyChat/MainPage/MainPage.tsx
@@ -4,6 +4,21 @@ import ChatDisplay from '../Components/ChatDisplay';
 
 const MainPage = () => {
 
+  const messages = [
+    {
+      chatname: 'Gloria',
+      lastmsg: 'How was your day?',
+      unread_messages: 1,
+      lastmsg_time: "1:00 am"
+    },
+    {
+      chatname: 'Math 101',
+      lastmsg: 'Can we have class by 1',
+      unread_messages: 7,
+      lastmsg_time: "10:00 pm"
+    }
+  ]
+
   return (
     <Box px="15px" width="100%" height="full">
       <Box mt="78px" h="24px" display="flex" alignItems="center"
@@ -17,7 +32,13 @@ const MainPage = () => {
         </Flex>
       </Box>
       <Box mt="20px" display="flex" alignItems="center" justifyContent="center" flexDir="column">
-        <ChatDisplay chatname="Math 101" lastmsg="Can we have a class by 1" unread_messages="7" lastmsg_time="10:00 pm" />
+        {
+          messages ? messages.map((message) => {
+            return (
+              <ChatDisplay chatname={message.chatname} lastmsg={message.lastmsg} unread_messages={message.unread_messages} lastmsg_time={message.lastmsg_time} />
+            )
+          }) : null
+        }
       </Box>
     </Box >
   )
