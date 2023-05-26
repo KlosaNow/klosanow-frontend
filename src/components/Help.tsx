2d50b52547421536c9b621ff2cfe98a3bc4dc2c8
Toma John Musa <tomajohn316@yahoo.com>
2023-05-26 22:47:18 +0100

    Refactor Help component to use chakra ui

--- src/components/Help.tsx
+++ src/components/Help.tsx
@@ -2,49 +2,51 @@ import Header from "./Header";
 import "../styles/help.scss";
 import { MdEmail, MdKeyboardArrowRight } from "react-icons/md";
 import { FaRobot } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link as ReachLink } from "react-router-dom";
+import { Box, Flex, Link, Text } from "@chakra-ui/react";
 
 const Help = () => {
   return (
     <>
       <Header pageName="Help" link="/" />
-      <section className="help">
-        <h2 className="heading2 page-heading">How can we help you?</h2>
 
-        <div className="help-content">
-          <Link to="/help/mail" className="help-items">
-            <div className="help-text">
-              <div className="icons">
+      <Box p="10px 6%">
+        <Text className="heading2" mb="20px">How can we help you?</Text>
+
+        <Flex direction="column" gap="26px" justify="center">
+          <Link as={ReachLink} w="100%" display="flex" justifyContent="space-between" gap="10px" to="/help/mail">
+            <Flex gap="7px" flex="80%">
+              <Box className="icons">
                 <MdEmail />
-              </div>
+              </Box>
 
-              <div>
-                <h3 className="heading3">Mail Us</h3>
-                <p className="p">For inquiries and complaints please send us a mail to help@klosanow.com</p>
-              </div>
-            </div>
-            <div className="icons">
+              <Box>
+                <Text pb="4px" className="heading3">Mail Us</Text>
+                <Text className="p">For inquiries and complaints please send us a mail to help@klosanow.com</Text>
+              </Box>
+            </Flex>
+            <Box className="icons">
               <MdKeyboardArrowRight />
-            </div>
+            </Box>
           </Link>
 
-          <Link to="/help/chat" className="help-items">
-            <div className="help-text">
-              <div className="icons">
+          <Link as={ReachLink} w="100%" display="flex" justifyContent="space-between" gap="10px" to="/help/chat">
+            <Flex gap="7px" flex="80%">
+              <Box className="icons">
                 <FaRobot />
-              </div>
+              </Box>
 
-              <div>
-                <h3 className="heading3">Chat with Bot</h3>
-                <p className="p">Resolve issue quicker by chatting with bot.</p>
-              </div>
-            </div>
-            <div className="icons">
+              <Box>
+                <Text pb="4px" className="heading3">Chat with Bot</Text>
+                <Text className="p">Resolve issue quicker by chatting with bot.</Text>
+              </Box>
+            </Flex>
+            <Box className="icons">
               <MdKeyboardArrowRight />
-            </div>
+            </Box>
           </Link>
-        </div>
-      </section>
+        </Flex>
+      </Box>
     </>
   )
 }
