2ec8fa93aaf1693661a619349b131f51a9e0570f
Toma John Musa <tomajohn316@yahoo.com>
2023-05-27 13:51:48 +0100

    Refactor Message component to use chakra Ui

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,4 +1,5 @@
 import { FaRobot } from "react-icons/fa";
+import { Box, Flex, Text } from "@chakra-ui/react";
 
 interface MessageOwner {
   owner: string;
@@ -10,23 +11,23 @@ const Message = (props:MessageOwner) => {
   const bot = (owner == "bot");
 
   return (
-    <article className={`message ${owner}`}>
-      <p className="p">{text}</p>
+    <Box w="80%" className={`${owner}`}>
+      <Text p="10px" borderRadius="10px" className="p">{text}</Text>
 
-      <div className="name-title">
+      <Flex align="center" justify="flex-start" fontSize="12px" fontWeight="400" lineHeight="15px" gap="3px" mt="8px">
         {bot && <>
-          <div className="bot-icon">
+          <Box p="4px 5px 6px" fontSize="15px" borderRadius="50%" color="neutral.50" bg="primary.50">
             <FaRobot />
-          </div>
-          <p>Klosanow</p>
+          </Box>
+          <Text>Klosanow</Text>
           </>
           }
 
           {
-            !bot && <p>You</p>
+            !bot && <Text>You</Text>
           }
-      </div>
-    </article>
+      </Flex>
+    </Box>
   )
 };
 
