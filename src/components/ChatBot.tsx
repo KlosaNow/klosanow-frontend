9dec8a3a8cf919ee79e895780ac8e9f117da743c
Toma John Musa <tomajohn316@yahoo.com>
2023-05-27 21:19:48 +0100

    Refactor chatbot component to use chakra ui

--- src/components/ChatBot.tsx
+++ src/components/ChatBot.tsx
@@ -2,6 +2,7 @@ import Header from "./Header";
 import { BsSend } from "react-icons/bs";
 import Message from "./Message";
 import { useEffect, useRef, useState } from "react";
+import { Box, Button, Flex, Input } from "@chakra-ui/react";
 
 interface Text {
   id: number;
@@ -59,26 +60,26 @@ const ChatBot = () => {
     return (<>
       <Header pageName="Chat with Bot" link="/help" />
 
-      <section className="chat">
-        <div className="chats">
-          <div className="scroll">
-            <div className="chat-flex">
-              <div className="scrollable" ref={scroll}>
+      <Flex direction="column" align="stretch" justify="flex-end" gap="20px" p="10px 6%" h="calc(100vh - 58px)">
+        <Box flex="calc(100% - 41px)" position="relative" overflowY="auto">
+          <Box position="absolute" top="0" left="0" right="0" bottom="0">
+            <Flex direction="column" align="stretch" justify="flex-end" gap="5px" w="100%" h="100%">
+              <Flex direction="column" gap="5px" align="stretch" w="100%" overflowY="auto" justify="flex-start" ref={scroll}>
                 { messages.map((message) => (
                   <Message owner={message.owner} text={message.text} key={message.id} />
                 ))}
-              </div>
-            </div>
-          </div>
-        </div>
+              </Flex>
+            </Flex>
+          </Box>
+        </Box>
 
-        <div className="chat-field">
-          <input type="text" placeholder="Type your message" onChange={handleInput} value={userInput} required />
+        <Flex align="center" justify="space-between" border="0.5px" borderColor="black.10" borderRadius="4px" borderStyle="solid" p="10px">
+          <Input variant="unstyled" w="90%" h="100%" type="text" placeholder="Type your message" onChange={handleInput} value={userInput} required />
           <button className="icon" onClick={sendMessage}>
             <BsSend />
           </button>
-        </div>
-      </section>
+        </Flex>
+      </Flex>
     </>
   );
 };
