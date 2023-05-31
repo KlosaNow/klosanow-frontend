7b6dbf305e76fe6901244e6009d497aa33b188bb
Odafe Alaiya <alaiyaodafe@gmail.com>
2023-05-31 03:34:29 +0100

    Merging with staging-2

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
 import { Route, Routes } from "react-router-dom";
-import { Error } from "./pages/ErrorPage";
+
+// Auth Pages
+import SignIn from "./pages/Auth/SignIn";
+import SignUp from "./pages/Auth/SignUp";
+import Otp from "./pages/Auth/Otp";
+
+import CreateLessonLayout from "./components/Layout/CreateLessonLayout";
+
+import Onboarding from "./pages/Onboarding";
 
 import { Notifications } from "./pages/Notifications";
 import { NotificationSettings } from "./pages/NotificationSettings";
 
+import CreateLesson from "./pages/CreateLesson";
+import LessonTemplate from "./pages/CreateLesson/LessonTemplate";
+
+import FreeStorageView from "./pages/StorageView/FreeStorageView";
+import BasicStorageView from "./pages/StorageView/BasicStorageView";
+import PremiumStorageView from "./pages/StorageView/PremiumStorageView";
+
+import { Error } from "./pages/ErrorPage";
 import { ChatPage } from "./pages/StudyChat";
-import Onboarding from "./pages/Onboarding";
 
 function App() {
   return (
     <>
       <Routes>
+
         <Route path="/" element={<Onboarding />} />
+
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/otp" element={<Otp />} />
+
+
+        <Route path="/create-lesson" element={<CreateLessonLayout />}>
+          <Route path="" index element={<CreateLesson />} />
+          <Route path="select-template" element={<LessonTemplate />} />
+        </Route>
+
+        <Route path="studychat" element={<ChatPage />} />
+
         <Route path="/notifications" element={<Notifications />} />
-        <Route
-          path="/settings/notifications"
-          element={<NotificationSettings />}
-        />
+        <Route path="/settings/notifications" element={<NotificationSettings />} />
+
+        {/* Storage view Route */}
+        <Route path="/free" element={<FreeStorageView />} />
+        <Route path="/basic" element={<BasicStorageView />} />
+        <Route path="/premium" element={<PremiumStorageView />} />
+
+        {/* this should always be the last route */}
         <Route path="*" element={<Error />} />
-        <Route path="/studychat" element={<ChatPage />} />
+
       </Routes>
     </>
   );
