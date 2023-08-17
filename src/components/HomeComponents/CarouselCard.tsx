dda286572b1efada2f69f68e45bfe1a202b9227f
Nwosu Joseph Chuks <chuksdj07@gmail.com>
2023-08-18 06:58:05 +0800

    built created and saved lesson component

--- src/components/HomeComponents/CarouselCard.tsx
+++ src/components/HomeComponents/CarouselCard.tsx
@@ -1,55 +1,27 @@
 import { FC } from "react";
-import { Container, VStack, Text, Box } from "@chakra-ui/react";
+import LessonCard from "../LessonCard/LessonCard";
 
 interface CarouselCardProps {
-  img: string;
+  thumbnail: string;
   title: string;
   timestamp: string;
   description: string;
 }
-export const CarouselCard: FC<CarouselCardProps> = ({
-  img,
+const CarouselCard: FC<CarouselCardProps> = ({
+  thumbnail,
   title,
   timestamp,
   description,
 }: CarouselCardProps) => {
+
   return (
-    <Container width="190px" padding={0}>
-      <VStack>
-        <Box
-          pos="relative"
-          padding={0}
-          borderRadius={5}
-          overflow="hidden"
-          width="100%"
-          height="150px"
-        >
-          <img
-            style={{ width: "100%", height: "150px", objectFit: "cover" }}
-            src={img}
-            alt=""
-          />
-          <Text
-            pos="absolute"
-            bottom={5}
-            right={5}
-            fontSize={12}
-            background="#000"
-            paddingY={1}
-            paddingX={4}
-            borderRadius={15}
-            color="#fff"
-          >
-            {timestamp}
-          </Text>
-        </Box>
-        <Text fontSize={16} fontWeight="600" color="#000">
-          {title}
-        </Text>
-        <Text fontSize={12} fontWeight="400" color="#000">
-          {description}
-        </Text>
-      </VStack>
-    </Container>
+    <LessonCard 
+        thumbnail={thumbnail}
+        title={title}
+        duration={timestamp}
+        description={description}
+      />
   );
 };
+
+export  default CarouselCard
