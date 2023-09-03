dcfd714ab04fbe197a7f42212002fcad4a33fbd9
Nwosu Joseph Chuks <chuksdj07@gmail.com>
2023-09-03 08:03:52 +0800

    added control btns

--- src/pages/CreateLesson/components/RecordLesson.tsx
+++ src/pages/CreateLesson/components/RecordLesson.tsx
@@ -1,10 +1,11 @@
 import  { FC } from 'react'
-import {  Box, Text, Image } from '@chakra-ui/react';
+import {  Box, Text, Image, Button } from '@chakra-ui/react';
+import { BsRecordCircleFill, BsPenFill, BsCursorFill } from "react-icons/bs";
 
 const RecordLesson:FC = () => {
   return (
     <Box>
-      <Box px="3" bg="#F8F7FE">
+      <Box px="3" bg="#F8F7FE" mb="5">
         <Box py={["0", "3"]}>
           <Text fontSize="32px" textColor="#DFA491">
             Chemical Compound
@@ -31,22 +32,55 @@ const RecordLesson:FC = () => {
             united in fixed proportions. The word "compound" also refers to any
             substance formed from two or more elements chemically united in
             fixed proportions. A compound is a mixture of two or more separate
-            elements.<br /> A chemical compound is a substance formed from two or more
+            elements.
+            <br /> A chemical compound is a substance formed from two or more
             elements chemically united in fixed proportions. A compound is a
             mixture of two or more separate elements. A chemical compound is a
             substance formed from two or more elements chemically united in
             fixed proportions. A compound is a mixture of two or more separate
-            elements. <br />A chemical compound is a substance formed from two or more
-            elements chemically united in fixed proportions.A compound is a
-            mixture of two or more separate elements. A chemical compound is a
-            substance formed from two or more elements chemically united in
+            elements. <br />A chemical compound is a substance formed from two
+            or more elements chemically united in fixed proportions.A compound
+            is a mixture of two or more separate elements. A chemical compound
+            is a substance formed from two or more elements chemically united in
             fixed proportions.A compound is a mixture of two or more separate
             elements. A chemical compound is a substance formed from two or more
             elements chemically united in fixed proportions.
           </Text>
         </Box>
       </Box>
-      <Box></Box>
+      <Box
+        width={["full", "60%"]}
+        mx="auto"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        py="2"
+        bg="white"
+        shadow="md"
+        borderRadius="md"
+        border="1px"
+        borderColor="gray.200"
+        gap="5"
+      >
+        <Button display="flex" flexDirection="column" bg="none" gap="2" py="1">
+          <BsRecordCircleFill color="red"  />
+          <Text fontSize="10px" textColor="#AAAAAA">
+            Record
+          </Text>
+        </Button>
+        <Button display="flex" flexDirection="column" bg="none" gap="2" py="1">
+          <BsPenFill color="#7B58F4" />
+          <Text fontSize="10px" textColor="#AAAAAA">
+            Pen Tool
+          </Text>
+        </Button>
+        <Button display="flex" flexDirection="column" bg="none" gap="2" py="1">
+          <BsCursorFill color="#7B58F4" />
+          <Text fontSize="10px" textColor="#AAAAAA">
+            Move Tool
+          </Text>
+        </Button>
+      </Box>
     </Box>
   );
 }
